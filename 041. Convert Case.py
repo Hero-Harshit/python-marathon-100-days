e8e1fcf6b69a63b1a85e58b4de1ae1e0dbdd01dd
Harshit <heroharshitlaptop@gmail.com>
2026-02-10 16:09:46 +0530

    I solved Convert Case problem

---
+++ 041. Convert Case.py
@@ -0,0 +1,6 @@
+# Convert Case
+# Take a string and convert it to completely uppercase and completely lowercase.
+
+text = input("Enter a string: ")
+print(f"Uppercase: {text.upper()}")
+print(f"Lowercase: {text.lower()}")
