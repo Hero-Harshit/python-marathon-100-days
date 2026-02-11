7bbbaedf5305619956aa73bfc94f1fe41d8a1a0a
Harshit <heroharshitlaptop@gmail.com>
2026-02-11 17:09:22 +0530

    I solved Remove Spaces problem

---
+++ 042. Remove Spaces.py
@@ -0,0 +1,6 @@
+# Remove Spaces
+# Remove all spaces from a user-entered string.
+
+text = input("Enter a string with spaces: ")
+no_spaces = text.replace(" ", "")
+print(f"String without spaces: {no_spaces}")
