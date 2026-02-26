83cf05de491e594a1f851757fcd759783c76ea50
Harshit <heroharshitlaptop@gmail.com>
2026-02-26 18:14:42 +0530

    I solved Reverse a List problem

---
+++ 057. Reverse a List.py
@@ -0,0 +1,8 @@
+# Reverse a List
+# Reverse the elements of a list.
+
+user_input = input("Enter items separated by spaces: ")
+items = user_input.split()
+
+items.reverse()
+print(f"Reversed list: {items}")
