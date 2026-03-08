725a39d8077128fb69a6c002f4e894afcbd8b94c
Harshit <heroharshitlaptop@gmail.com>
2026-03-08 22:02:12 +0530

    I solved Tuple Unpacking problem

---
+++ 067. Tuple Unpacking.py
@@ -0,0 +1,11 @@
+# Tuple Unpacking
+# Store a person's name, age, and city in a tuple and unpack them into separate variables.
+
+person_info = ("Alice", 25, "New York")
+
+# Unpacking
+name, age, city = person_info
+
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"City: {city}")
